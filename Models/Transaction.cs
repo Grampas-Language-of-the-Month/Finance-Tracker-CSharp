97bbb786b2575c17da35fa8d61c6eaf25bc58425
Josh Walker <walker11710@gmail.com>
2026-02-07 16:24:16 -0700

    Add transaction Model

---
+++ Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace finance_tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime TransactionDate { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string ExternalReference { get; set; }
+    }
+}
